65b6fe493ee5017817a5c8dfe694dddc8a001ae1
Pratap Raj Srivastava <pratapraj056@gmail.com>
2017-04-19 19:50:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/prata/classmate/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.prata.classmate;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.prata.classmate.sugarrecord.TimeTable;
+import com.orm.SugarContext;
+
+public class MainActivity extends AppCompatActivity {
+	
+	private TextView mTextMessage;
+	
+	private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+			= new BottomNavigationView.OnNavigationItemSelectedListener() {
+		
+		@Override
+		public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+			switch (item.getItemId()) {
+				case R.id.navigation_day1:
+					mTextMessage.setText(R.string.title_day1);
+					return true;
+				case R.id.navigation_day2:
+					mTextMessage.setText(R.string.title_day2);
+					return true;
+				case R.id.navigation_day3:
+					mTextMessage.setText(R.string.title_day3);
+					return true;
+				case R.id.navigation_day4:
+					mTextMessage.setText(R.string.title_day4);
+					return true;
+				case R.id.navigation_day5:
+					mTextMessage.setText(R.string.title_day5);
+					return true;
+			}
+			return false;
+		}
+		
+	};
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		SugarContext.init(this);
+		
+		
+		TimeTable timeTable = new TimeTable(1, "A", "A", "F", "F", "D" ,"na", "na", "na", "na", "na");
+		timeTable.save();
+		TimeTable timeTable2 = new TimeTable(2, "na", "na", "na", "na", "na" ,"B", "B", "G", "G", "A");
+		timeTable2.save();
+		TimeTable timeTable3 = new TimeTable(3, "C", "C", "E", "F", "D" ,"na", "na", "na", "na", "na");
+		timeTable3.save();
+		TimeTable timeTable4 = new TimeTable(4, "na", "na", "na", "na", "na" ,"D", "D", "B", "C", "G");
+		timeTable4.save();
+		TimeTable timeTable5 = new TimeTable(5, "E", "E", "A", "B", "C" ,"na", "na", "na", "na", "na");
+		timeTable5.save();
+		
+		mTextMessage = (TextView) findViewById(R.id.message);
+		BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+		navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+	}
+	
+}
---
+++ app/src/main/java/com/example/prata/classmate/sugarrecord/TimeTable.java
@@ -0,0 +1,38 @@
+package com.example.prata.classmate.sugarrecord;
+
+import com.orm.SugarRecord;
+import com.orm.dsl.Unique;
+
+public class TimeTable extends SugarRecord{
+	@Unique
+	int id;
+	String p1;
+	String p2;
+	String p3;
+	String p4;
+	String p5;
+	String p6;
+	String p7;
+	String p8;
+	String p9;
+	String p10;
+	
+	public TimeTable(){
+		
+	}
+	
+	
+	public TimeTable(int id, String p1, String p2, String p3, String p4, String p5, String p6, String p7, String p8, String p9, String p10) {
+		this.id = id;
+		this.p1 = p1;
+		this.p2 = p2;
+		this.p3 = p3;
+		this.p4 = p4;
+		this.p5 = p5;
+		this.p6 = p6;
+		this.p7 = p7;
+		this.p8 = p8;
+		this.p9 = p9;
+		this.p10 = p10;
+	}
+}
